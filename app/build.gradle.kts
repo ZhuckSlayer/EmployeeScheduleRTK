7bdce4926ed7a22b34aa9019b882216bfb66cbdb
ZhuckSlayer <monstr289@list.ru>
2024-05-21 17:19:25 +0700

    initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id ("kotlin-kapt")
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.example.employeeschedulertk"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.employeeschedulertk"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures{
+        viewBinding=true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    // LiveData
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    // ViewModel
+    implementation(libs.lifecycle.viewmodel.ktx)
+
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
+
+    implementation (libs.androidx.room.runtime)
+    annotationProcessor (libs.androidx.room.compiler)
+    implementation (libs.dagger)
+    kapt (libs.dagger.android.processor)
+    kapt (libs.dagger.compiler)
+
+    implementation (libs.androidx.navigation.fragment.ktx)
+    implementation (libs.androidx.navigation.ui.ktx)
+
+
+    implementation (libs.firebase.auth)
+    implementation (libs.firebase.database)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
